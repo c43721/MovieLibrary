1b852c4b3425a910e8aa40d57ad0b8821305c6dd
c43721 <rlahman.game@gmail.com>
2020-10-13 18:55:24 -0500

    Add cors and bodyparser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const db = require("./db/repository-wrapper");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 3006;
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.use(cors());
+app.use(express.json());
