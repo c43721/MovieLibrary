5bed6f614b5a7cc54fbbbc923cfb3d33a7672d0a
c43721 <rlahman.game@gmail.com>
2020-10-20 17:07:06 -0500

    Edit movie function + frontend style the form

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,46 +1,40 @@
 "use strict";
 
-//have constants for maybe base api url, and a local movie array to be overridden.
+//Local constant for API url
 const baseApiUrl = "http://localhost:3006/api";
 
-//use AJAX to get all the movies, on page load
-//Loop through each of those movies, and make a new "card" for each movie in the list. Each card should be tailored to show the movie details.
+//On load, get all movies
+getMovies();
 
-//create movie function/button should be able to send a request to the API, then it should return the new list. That new list should override the previous list, and update every entry again.
-
-//update movie function/button should be able to take a form body, and send a PUT request to the API, which will update that movie, and then that new list should call a function to update the movies again
-
-//functions:
-//a function to GET all movies
-//a function to update (PUT) a movie in the list
-//a function to create a new movie, then that function would need to call the "GET all movies" function to update the data on the frontend
-
-function updateSpecificMovie(id, data) {
-    $.ajax({
+async function updateSpecificMovie(id, data) {
+    return await $.ajax({
         type: "PUT",
         url: baseApiUrl + "/update/" + id,
         contentType: 'application/json',
         data: JSON.stringify(data),
-        success: function (data) {
-            //Do something to modify the frontend
-            console.log(data);
-        }
     })
 }
 
 function getMovies() {
     $.ajax({
-        type: "GEt",
+        type: "GET",
         url: baseApiUrl + "/movies",
         contentType: 'application/json',
         success: function (data) {
-            console.log(data);
             updateCards(data);
         }
     })
 }
 
-getMovies();
+async function getSpecificMovie(id) {
+    const output = await $.ajax({
+        type: "GET",
+        url: baseApiUrl + "/movies/" + id,
+        contentType: 'application/json',
+    })
+    return output;
+}
+
 
 function updateCards(movieArray) {
     const container = $("#cards-container");
@@ -53,15 +47,56 @@ function updateCards(movieArray) {
                 <h5 class="card-title">${movie.title}</h5>
                     <p class="card-text">Director: ${movie.director}</p>
                     <p class="card-text">Genre: ${movie.genre}</p>
+                    <a onclick="editMovie(${movie.id})" class="btn btn-primary">Edit</a>
                 </div>
             </div>
         `);
     }
 }
 
-// updateSpecificMovie(1, {
-//     "id": 1,
-//     "title": "The Departed 2",
-//     "director": "Martin Scorsese",
-//     "genre": "Drama"
-// })
+async function editMovie(id) {
+    const movieFromDb = await getSpecificMovie(id);
+
+    $("#edit").empty().append(`
+    <form>
+        <div class="form-group">
+            <label for="id">ID</label>
+            <input type="email" class="form-control disabled" id="id" aria-describedby="id" placeholder="${id}" readonly>
+        </div>
+        <div class="form-group">
+            <label for="newMovieName">Name</label>
+            <input type="text" class="form-control" id="newMovieName" aria-describedby="movieName" placeholder="${movieFromDb.title}">
+        </div>
+        <div class="form-group">
+            <label for="newMovieDirector">Director</label>
+            <input type="text" class="form-control" id="newMovieDirector" aria-describedby="emailHelp" placeholder="${movieFromDb.director}">
+        </div>
+        <div class="form-group">
+            <label for="newMovieGenre">Genre</label>
+            <input type="text" class="form-control" id="newMovieGenre" aria-describedby="emailHelp" placeholder="${movieFromDb.genre}">
+        </div>
+    <button type="submit" class="btn btn-primary">Edit</button>
+    <button type="button" class="btn btn-secondary" onclick="cancelEditMovie()">Cancel</button>
+    </form>
+    `).on("submit", async function (e) {
+        e.preventDefault();
+
+        const newTitle = $("#newMovieName").val() || movieFromDb.title;
+        const newDirector = $("#newMovieDirector").val() || movieFromDb.director;
+        const newGenre = $("#newMovieGenre").val() || movieFromDb.genre;
+
+        await updateSpecificMovie(id, {
+            id,
+            title: newTitle,
+            director: newDirector,
+            genre: newGenre
+        })
+
+        getMovies();
+        $("#edit").empty();
+    })
+}
+
+function cancelEditMovie() {
+    return $("#edit").empty();
+}
