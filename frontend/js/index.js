87662d610f40cc999874d2dfe6a6c8d4b337803e
c43721 <rlahman.game@gmail.com>
2020-10-20 17:42:29 -0500

    Modal the edit page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -58,45 +58,60 @@ async function editMovie(id) {
     const movieFromDb = await getSpecificMovie(id);
 
     $("#edit").empty().append(`
-    <form>
-        <div class="form-group">
-            <label for="id">ID</label>
-            <input type="email" class="form-control disabled" id="id" aria-describedby="id" placeholder="${id}" readonly>
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">${movieFromDb.title}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                    <label for="id">ID</label>
+                    <input type="email" class="form-control disabled" id="id" aria-describedby="id" placeholder="${id}" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="newMovieName">Name</label>
+                    <input type="text" class="form-control" id="newMovieName" aria-describedby="movieName" placeholder="${movieFromDb.title}">
+                </div>
+                <div class="form-group">
+                    <label for="newMovieDirector">Director</label>
+                    <input type="text" class="form-control" id="newMovieDirector" aria-describedby="emailHelp" placeholder="${movieFromDb.director}">
+                </div>
+                <div class="form-group">
+                    <label for="newMovieGenre">Genre</label>
+                    <input type="text" class="form-control" id="newMovieGenre" aria-describedby="emailHelp" placeholder="${movieFromDb.genre}">
+                </div>
+                <button type="submit" class="btn btn-primary">Edit</button>
+                </form>
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div> 
         </div>
-        <div class="form-group">
-            <label for="newMovieName">Name</label>
-            <input type="text" class="form-control" id="newMovieName" aria-describedby="movieName" placeholder="${movieFromDb.title}">
-        </div>
-        <div class="form-group">
-            <label for="newMovieDirector">Director</label>
-            <input type="text" class="form-control" id="newMovieDirector" aria-describedby="emailHelp" placeholder="${movieFromDb.director}">
-        </div>
-        <div class="form-group">
-            <label for="newMovieGenre">Genre</label>
-            <input type="text" class="form-control" id="newMovieGenre" aria-describedby="emailHelp" placeholder="${movieFromDb.genre}">
-        </div>
-    <button type="submit" class="btn btn-primary">Edit</button>
-    <button type="button" class="btn btn-secondary" onclick="cancelEditMovie()">Cancel</button>
-    </form>
+    </div>
     `).on("submit", async function (e) {
+        console.log("submitted");
         e.preventDefault();
 
+        const editingId = movieFromDb.id;
         const newTitle = $("#newMovieName").val() || movieFromDb.title;
         const newDirector = $("#newMovieDirector").val() || movieFromDb.director;
         const newGenre = $("#newMovieGenre").val() || movieFromDb.genre;
 
         await updateSpecificMovie(id, {
-            id,
+            id: editingId,
             title: newTitle,
             director: newDirector,
             genre: newGenre
         })
 
         getMovies();
-        $("#edit").empty();
     })
-}
 
-function cancelEditMovie() {
-    return $("#edit").empty();
+    $('#editModal').modal();
 }
